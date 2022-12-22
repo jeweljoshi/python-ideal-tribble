94e17781ec9b687f226d491dca700de3671d3e71
Jewel Joshi <103859244+jeweljoshi@users.noreply.github.com>
2022-12-23 03:13:32 +0400

    Create check_positive_or_negative_number.py

---
+++ check_positive_or_negative_number.py
@@ -0,0 +1,8 @@
+# Get the input from the user
+num = int(input("Enter an integer: "))
+
+# Check if the number is positive or negative
+if num >= 0:
+    print(f"{num} is a positive number.")
+else:
+    print(f"{num} is a negative number.")
